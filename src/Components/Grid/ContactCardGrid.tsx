9f3465a3d02d10e977b58f9203dd357b774002b8
Taras.Kravets <taraskravets95@gmail.com>
2023-03-12 12:27:59 +0200

    feat(Router): added diff routes

---
+++ src/Components/Grid/ContactCardGrid.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ContactCardGrid() {
+	return <div>Contact Card Grid</div>;
+}
--- src/Components/Scaffold/NavDrawer.tsx
+++ src/Components/Scaffold/NavDrawer.tsx
@@ -1,32 +1,50 @@
 import {
-  AppBar,
-  Drawer,
-  List,
-  ListItem,
-  Toolbar,
-  Typography,
+	AppBar,
+	Drawer,
+	List,
+	ListItem,
+	Toolbar,
+	Typography,
 } from '@mui/material';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import ContactDataGrid from '../DataGrid/ContactDataGrid';
+import ContactCardGrid from '../DataGrid/ContactDataGrid';
+import ContactForm from '../Form/ContactForm';
+import ContactTable from '../Table/ContactTable';
 
 export default function NavDrawer() {
-  return (
-    <div>
-      <AppBar position='fixed'>
-        <Toolbar>
-          <Typography variant='h6' noWrap>
-            MUI Styling
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer variant='temporary' open={true}>
-        <List>
-          {[{ text: 'Input Form' }, { text: 'Contact Card Grid' }].map(
-            (nav, index) => (
-              <ListItem key={nav.text}>{nav.text}</ListItem>
-            )
-          )}
-        </List>
-      </Drawer>
-      <main></main>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<AppBar position='fixed'>
+				<Toolbar>
+					<Typography variant='h6' noWrap>
+						MUI Styling
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<Drawer variant='temporary' open={true}>
+				<List>
+					{[
+						{ text: 'Input Form', route: '/form' },
+						{ text: 'Contact Card Grid', route: '/grid' },
+						{ text: 'Contact Table', route: '/table' },
+						{ text: 'Contact Data Grid', route: '/data-grid' },
+					].map((nav, index) => (
+						<ListItem key={nav.text}>
+							<Link to={nav.route}>{nav.text}</Link>
+						</ListItem>
+					))}
+				</List>
+			</Drawer>
+			<main>
+				<Routes>
+					<Route path='/' element={<ContactForm />} />
+					<Route path='/form' element={<ContactForm />} />
+					<Route path='/grid' element={<ContactCardGrid />} />
+					<Route path='/table' element={<ContactTable />} />
+					<Route path='/datagrida' element={<ContactDataGrid />} />
+				</Routes>
+			</main>
+		</BrowserRouter>
+	);
 }
