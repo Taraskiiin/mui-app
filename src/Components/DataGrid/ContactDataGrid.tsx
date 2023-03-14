a46a3e63919fef948c5ed743feadbadb3e93accb
Taras.Kravets <taraskravets95@gmail.com>
2023-03-14 16:46:19 +0200

    feat(ContactDateGrid): added Contact Date Grid

--- src/Components/DataGrid/ContactDataGrid.tsx
+++ src/Components/DataGrid/ContactDataGrid.tsx
@@ -1,5 +1,66 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
+import { useTheme, Theme } from '@mui/material/styles';
+import { contactData } from '../../Data/ContactData';
+
+const columns = (theme: Theme) => [
+	{
+		field: 'name',
+		headerName: 'Name',
+		minWidth: 150,
+		renderCell: (cellValues: GridRenderCellParams<string>) => {
+			return cellValues.value;
+		},
+	},
+	{
+		field: 'role',
+		headerName: 'Role',
+		minWidth: 100,
+		renderCell: (cellValues: GridRenderCellParams<string>) => {
+			return cellValues.value;
+		},
+	},
+	{
+		field: 'skills',
+		headerName: 'Skills',
+		minWidth: 150,
+		renderCell: (cellValues: GridRenderCellParams<string>) => {
+			return (
+				<div style={{ color: theme.palette.primary.main }}>
+					{cellValues.value && cellValues.value[0]}
+				</div>
+			);
+		},
+	},
+	{
+		field: 'lastUpdates',
+		headerName: 'Last Updates',
+		minWidth: 120,
+		renderCell: (cellValues: GridRenderCellParams<string>) => {
+			return cellValues.value;
+		},
+	},
+	{
+		field: 'preference',
+		headerName: 'Work Preference',
+		minWidth: 150,
+		renderCell: (cellValues: GridRenderCellParams<string>) => {
+			return cellValues.value;
+		},
+	},
+];
 
 export default function ContactDataGrid() {
-	return <>Contact Data Grid</>;
+	const rows = () => [...contactData];
+	const theme = useTheme();
+	return (
+		<DataGrid
+			autoHeight
+			rows={rows()}
+			columns={columns(theme)}
+			pageSize={5}
+			headerHeight={60}
+			rowHeight={120}
+		/>
+	);
 }
