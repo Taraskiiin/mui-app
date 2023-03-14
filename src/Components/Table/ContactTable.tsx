06b7b0bbcd4702405bb7c8e41b158090945fc0d6
Taras.Kravets <taraskravets95@gmail.com>
2023-03-14 15:16:30 +0200

    feat(contactTable): added ContactTable component

--- src/Components/Table/ContactTable.tsx
+++ src/Components/Table/ContactTable.tsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import {
+	Table,
+	TableBody,
+	TableContainer,
+	TableCell,
+	TableHead,
+	TableRow,
+} from '@mui/material';
+import { contactData } from '../../Data/ContactData';
 
 export default function ContactTable() {
-    return <div>Contact Table</div>
+	return (
+		<TableContainer>
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableCell>Name</TableCell>
+						<TableCell>Role</TableCell>
+						<TableCell>Skills</TableCell>
+						<TableCell>Last updates</TableCell>
+						<TableCell>Preference</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{contactData.map((contact) => (
+						<TableRow key={contact.name}>
+							{Object.entries(contact).map(
+								([key, value]) =>
+									key !== 'id' && <TableCell key={key}>{value}</TableCell>
+							)}
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
 }
